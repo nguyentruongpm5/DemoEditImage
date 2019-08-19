d9c90824482e7af8ee57dd3519053bcf73e97c8d
nguyentruongpm5 <missga97@gmail.com>
2019-08-19 14:04:22 +0700

    Chi tiết sản phẩm

--- app/src/main/java/com/example/demoeditimage/activity/ProductDetailActivity.java
+++ app/src/main/java/com/example/demoeditimage/activity/ProductDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.demoeditimage.activity;
 
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.demoeditimage.R;
 
@@ -11,7 +14,8 @@ import butterknife.OnClick;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
-
+    @BindView(R.id.capture_image_layout)
+    LinearLayout capture_image_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,24 @@ public class ProductDetailActivity extends AppCompatActivity {
     @OnClick(R.id.btnBack)
     void clickToBack (){
         onBackPressed();
+    }
+
+    @OnClick (R.id.capture_image_layout)
+    void detailProduct (){
+        showDialog();
+    }
+
+    private void showDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Thông báo")
+                .setMessage("Bạn có muốn thoát ra khỏi ứng dụng không ?")
+                .setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Không", null)
+                .show();
     }
 }
