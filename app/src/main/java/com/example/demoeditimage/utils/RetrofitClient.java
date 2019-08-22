e71edc4dd6f5672ce317540f92826c00009c9f31
nguyentruongpm5 <missga97@gmail.com>
2019-08-22 17:05:33 +0700

    Retrofit Client

--- app/src/main/java/com/example/demoeditimage/utils/RetrofitClient.java
+++ app/src/main/java/com/example/demoeditimage/utils/RetrofitClient.java
@@ -1,6 +1,8 @@
 package com.example.demoeditimage.utils;
 
 import com.example.demoeditimage.interfaces.CallApiRegistration;
+import com.example.demoeditimage.interfaces.CallLocalAPI;
+import com.google.gson.Gson;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -16,5 +18,13 @@ public class RetrofitClient {
         return retrofit.create(CallApiRegistration.class);
     }
 
+    public CallLocalAPI getCallLocalAPI() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(CallLocalAPI.BASE_URL_local)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(CallLocalAPI.class);
+    }
 
 }
