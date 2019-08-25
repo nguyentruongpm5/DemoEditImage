43d8e5c14bc4217de22ed3efc43b8ea694268ba5
nguyentruongpm5 <missga97@gmail.com>
2019-08-25 19:09:08 +0700

    aaa
    Signed-off-by: nguyentruongpm5 <missga97@gmail.com>

--- app/src/main/java/com/example/demoeditimage/adapter/ProductItemAdapter.java
+++ app/src/main/java/com/example/demoeditimage/adapter/ProductItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.demoeditimage.adapter;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -33,35 +34,39 @@ public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View itemView = LayoutInflater.from(viewGroup.getContext()).
-                inflate(R.layout.content_item_product,viewGroup,false);
+                inflate(R.layout.content_item_product, viewGroup, false);
         ViewHolder vh = new ViewHolder(itemView);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
-        Product product= productItemList.get(i);
+        Product product = productItemList.get(i);
 
 //        viewHolder.image_product.setImageBitmap();
 
         viewHolder.txtProductName.setText(product.getName());
 
-        switch (product.getStatus()){
-            case "NORMAL" :
+        switch (product.getStatus()) {
+            case "NORMAL":
+                viewHolder.txtStatus.setTextColor(Color.rgb(0, 153, 51));
                 viewHolder.txtStatus.setText("Hiển thị");
                 break;
-            case "DELETED" :
+            case "DELETED":
+                viewHolder.txtStatus.setTextColor(Color.rgb(0, 137, 255));
                 viewHolder.txtStatus.setText("Đã xóa");
                 break;
-            case "BANNED" :
+            case "BANNED":
+                viewHolder.txtStatus.setTextColor(Color.RED);
                 viewHolder.txtStatus.setText("Khóa");
                 break;
-            case "UNLIST" :
+            case "UNLIST":
+                viewHolder.txtStatus.setTextColor(Color.GRAY);
                 viewHolder.txtStatus.setText("Ẩn");
                 break;
         }
 
-        if (product.getImages()!= null) {
+        if (product.getImages() != null) {
             Picasso.get()
                     .load(product.getImages()[0])
                     .placeholder(R.mipmap.placeholder_images)
@@ -99,7 +104,7 @@ public class ProductItemAdapter extends RecyclerView.Adapter<ProductItemAdapter.
         public ViewHolder(@NonNull View itemView) {
 
             super(itemView);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 
